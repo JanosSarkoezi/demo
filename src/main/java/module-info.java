5e762817e08a791824035556596567904a76a3f1
Janos Sarkoezi <j.sarkoezi@googlemail.com>
2026-01-20 22:53:05 +0100

    Module Info angepasst

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,16 @@ module com.example.demo {
     opens com.example.demo.app to javafx.fxml;
     exports com.example.demo.controller;
     opens com.example.demo.controller to javafx.fxml;
+    exports com.example.demo.diagram.connection;
+    opens com.example.demo.diagram.connection to javafx.fxml;
+    exports com.example.demo.diagram.shape;
+    opens com.example.demo.diagram.shape to javafx.fxml;
+    exports com.example.demo.model;
+    opens com.example.demo.model to javafx.fxml;
+    exports com.example.demo.tool;
+    opens com.example.demo.tool to javafx.fxml;
+    exports com.example.demo.tool.state;
+    opens com.example.demo.tool.state to javafx.fxml;
+    exports com.example.demo.ui;
+    opens com.example.demo.ui to javafx.fxml;
 }
